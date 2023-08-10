b3f7c742b9b691f74345ea420344ab39b15c1ea1
ankulbbcspl <ankulparmar.bbcspl@gmail.com>
2023-08-10 10:28:42 +0530

    New Master Initial Commit

--- app/src/main/java/com/example/maarevacommoditytradingapp/retrofitApi/OurRetrofit.kt
+++
@@ -1,9 +0,0 @@
-package com.example.maarevacommoditytradingapp.retrofitApi
-
-
-interface OurRetrofit {
-//
-//    @Headers("Content-Type:application/json")
-//    @POST("/HRMSAPI/HRMSApi/HRMSApi/TaskMasterRtr")
-//    suspend fun getTaskMaster(@Body body: JsonObject): Response<TaskMasterRtrMasterModel>
-}
