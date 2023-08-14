3de06b2c9a039704afc137aae2f42c00e3321e0c
ankulbbcspl <ankulparmar.bbcspl@gmail.com>
2023-08-14 18:20:27 +0530

    PCAAuctionFragment.kt Design IN-PROGRESS

---
+++ app/src/main/java/com/bluebellcspl/maarevacommoditytradingapp/fragment/pca/PCAAuctionFragment.kt
@@ -0,0 +1,20 @@
+package com.bluebellcspl.maarevacommoditytradingapp.fragment.pca
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bluebellcspl.maarevacommoditytradingapp.R
+
+
+class PCAAuctionFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_p_c_a_auction, container, false)
+    }
+}
