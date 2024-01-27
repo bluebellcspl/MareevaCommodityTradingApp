4c5ac6d26de4a922d085da460f6a1e72d2cc78eb
ankulbbcspl <ankulparmar.bbcspl@gmail.com>
2024-01-27 10:43:50 +0530

    FileDownload Functionality Added

---
+++ app/src/main/java/com/bluebellcspl/maarevacommoditytradingapp/commonFunction/FileDownloader.kt
@@ -0,0 +1,56 @@
+package com.bluebellcspl.maarevacommoditytradingapp.commonFunction
+
+import android.app.DownloadManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.Uri
+import android.os.Environment
+import android.util.Log
+import android.widget.Toast
+
+class FileDownloader private constructor(private val context: Context) {
+    val TAG = "FileDownloader"
+    fun downloadFile(fileUrl: String, fileName: String) {
+        Log.d(TAG, "downloadFile: URL : $fileUrl")
+        Log.d(TAG, "downloadFile: FILENAME : $fileName")
+        val request = DownloadManager.Request(Uri.parse(fileUrl))
+        request.setTitle("Downloading $fileName")
+        request.setDescription("Downloading Report")
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+        // Set the destination directory and file name
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName)
+
+        // Enqueue the download
+        val downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        val downloadId = downloadManager.enqueue(request)
+
+        // Register a BroadcastReceiver to receive download complete notification
+        val onCompleteReceiver = MyDownloadReceiver(downloadId)
+        val filter = IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE)
+        context.registerReceiver(onCompleteReceiver, filter)
+
+        Toast.makeText(context, "Downloading $fileName", Toast.LENGTH_SHORT).show()
+    }
+
+    // BroadcastReceiver to handle download complete event
+    inner class MyDownloadReceiver(private val downloadId: Long) : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1) == downloadId) {
+                Toast.makeText(context, "Download complete!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: FileDownloader? = null
+
+        fun getInstance(context: Context): FileDownloader {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: FileDownloader(context).also { INSTANCE = it }
+            }
+        }
+    }
+}
--- app/src/main/java/com/bluebellcspl/maarevacommoditytradingapp/fragment/buyer/BuyerPreviousAuctionFragment.kt
+++ app/src/main/java/com/bluebellcspl/maarevacommoditytradingapp/fragment/buyer/BuyerPreviousAuctionFragment.kt
@@ -4,14 +4,24 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bluebellcspl.maarevacommoditytradingapp.R
 import com.bluebellcspl.maarevacommoditytradingapp.adapter.BuyerPrevAuctionAdapter
 import com.bluebellcspl.maarevacommoditytradingapp.commonFunction.CommonUIUtility
+import com.bluebellcspl.maarevacommoditytradingapp.commonFunction.DateUtility
+import com.bluebellcspl.maarevacommoditytradingapp.commonFunction.FileDownloader
+import com.bluebellcspl.maarevacommoditytradingapp.commonFunction.PrefUtil
+import com.bluebellcspl.maarevacommoditytradingapp.constants.URLHelper
 import com.bluebellcspl.maarevacommoditytradingapp.databinding.FragmentBuyerPreviousAuctionBinding
 import com.bluebellcspl.maarevacommoditytradingapp.master.FetchBuyerPreviousAuctionAPI
 import com.bluebellcspl.maarevacommoditytradingapp.model.BuyerPrevAuctionMasterModel
@@ -22,6 +32,8 @@ class BuyerPreviousAuctionFragment : Fragment() {
     val TAG = "BuyerPreviousAuctionFragment"
     lateinit var binding: FragmentBuyerPreviousAuctionBinding
     private val navArgs by navArgs<BuyerPreviousAuctionFragmentArgs>()
+    private val fileDownloader by lazy { FileDownloader.getInstance(requireContext()) }
+    lateinit var menuHost: MenuHost
     var PREV_AUCTION_SELECTED_DATE = ""
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,12 +47,35 @@ class BuyerPreviousAuctionFragment : Fragment() {
             false
         )
         PREV_AUCTION_SELECTED_DATE = navArgs.selectedPreviousAuctionDate
+        if (PREV_AUCTION_SELECTED_DATE.isBlank() || PREV_AUCTION_SELECTED_DATE.equals(""))
+        {
+            PREV_AUCTION_SELECTED_DATE = DateUtility().getCompletionDate()
+        }
         if (ConnectionCheck.isConnected(requireContext()))
         {
             FetchBuyerPreviousAuctionAPI(requireContext(),this@BuyerPreviousAuctionFragment,PREV_AUCTION_SELECTED_DATE)
         }else{
             commonUIUtility.showToast(getString(R.string.no_internet_connection))
         }
+
+        menuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.file_download_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                when (menuItem.itemId) {
+                    R.id.btn_DownloadReport -> {
+                     downloadAuctionReport()
+                    }
+                    R.id.btn_DownloadDetailReport->{
+                        downloadAuctionDetailReport()
+                    }
+                }
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.STARTED)
         return binding.root
     }
 
@@ -55,6 +90,32 @@ class BuyerPreviousAuctionFragment : Fragment() {
         } catch (e: Exception) {
             e.printStackTrace()
             Log.e(TAG, "bindDataOfPrevAuction: ${e.message}", )
+        }
+    }
+
+    fun downloadAuctionReport()
+    {
+        try {
+            val fileURL = URLHelper.BUYER_AUCTION_REPORT.replace("<COMMODITY_ID>",PrefUtil.getString(PrefUtil.KEY_COMMODITY_ID,"").toString()).replace("<DATE>",
+                PREV_AUCTION_SELECTED_DATE).replace("<COMPANY_CODE>",PrefUtil.getString(PrefUtil.KEY_COMPANY_CODE,"").toString()).replace("<BUYER_REG_ID>",PrefUtil.getString(PrefUtil.KEY_REGISTER_ID,"").toString())
+
+            fileDownloader.downloadFile(fileURL,"Auction_Report_$PREV_AUCTION_SELECTED_DATE.xlsx")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.e(TAG, "downloadAuctionReport: ${e.message}")
+        }
+    }
+
+    fun downloadAuctionDetailReport()
+    {
+        try {
+            val fileUrl = URLHelper.BUYER_AUCTION_DETAIL_REPORT.replace("<COMMODITY_ID>",PrefUtil.getString(PrefUtil.KEY_COMMODITY_ID,"").toString()).replace("<DATE>",
+                PREV_AUCTION_SELECTED_DATE).replace("<COMPANY_CODE>",PrefUtil.getString(PrefUtil.KEY_COMPANY_CODE,"").toString()).replace("<BUYER_REG_ID>",PrefUtil.getString(PrefUtil.KEY_REGISTER_ID,"").toString())
+
+            fileDownloader.downloadFile(fileUrl,"Auction_Detail_Report_$PREV_AUCTION_SELECTED_DATE.xlsx")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.e(TAG, "downloadAuctionDetailReport: ${e.message}")
         }
     }
 }
